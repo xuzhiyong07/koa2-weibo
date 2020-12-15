4eac843aabac96bc2f8b5d9f2d229a3fa397b082
mars <xuzhiyong1102@gmail.com>
2020-12-15 15:04:17 +0800

    feat: 测试路由及ejs模板

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,17 +2,50 @@ const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Koa 2!',
+    message: 'test demo',
+    isMe: false,
+    blogList: [
+      {
+        id: 1,
+        title: 'a'
+      },
+      {
+        id: 2,
+        title: 'b'
+      },
+      {
+        id: 3,
+        title: 'c'
+      },
+      {
+        id: 4,
+        title: 'd'
+      }
+    ]
   })
-})
-
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
 })
 
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:userName', async (ctx, next) => {
+  const { userName } = ctx.params
+  ctx.body = {
+    title: 'profile API',
+    userName
+  }
+})
+
+router.get('/loadMore/:userName/:pageIndex', async (ctx, next) => {
+  const { userName, pageIndex } = ctx.params
+  ctx.body = {
+    title: 'loadMore API',
+    userName,
+    pageIndex
   }
 })
 
